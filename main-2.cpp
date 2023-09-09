95d65a5051ad610ea2bb6c4e7957828096313f5b
jakerman777 <115594652+jakerman777@users.noreply.github.com>
2023-09-08 21:34:05 -0300

    Update main-2.cpp

--- main-2.cpp
+++ main-2.cpp
@@ -1,155 +1,158 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 using namespace std;
 
 struct Naipe {
-    int numero;
-    char palo;
-    char color;
+  int numero;
+  char palo;
+  char color;
 };
 
-/****************************************
-* Naipe * crearBaraja
-* **************************************
-* Lee el archivo baraja.dat y genera un arreglo de naipes (baraja)
-* **************************************
-*  Input:
-*       No tiene
-* **************************************
-* Returns:
-*       Naipe arregloBaraja[i], con i siendo un arreglo de naipes que será usado como
-*       baraja.
-***************************************/
+Naipe *crear_baraja(int &cartas_disponibles) {
+  ifstream archivo_entrada;
+  archivo_entrada.open("baraja.dat", ios::binary);
 
-Naipe *crear_baraja () {
-    ifstream archivo_entrada;
-    archivo_entrada.open("baraja.dat", ios::binary);
-    
-    // leer la cantidad de naipes en el archivo
-    int i;
-    archivo_entrada.read((char*)&i, sizeof(int));
+  // Leer la cantidad de naipes en el archivo
+  archivo_entrada.read((char *)(&cartas_disponibles), sizeof(int));
 
-    Naipe arregloBaraja[i];
-    Naipe naipeActual;
+  Naipe *arregloBaraja = new Naipe[cartas_disponibles];
 
-    for (int j = 0; j < i; j++) {
-        archivo_entrada.read((char*)&naipeActual, sizeof(Naipe));
-        arregloBaraja[j].color = naipeActual.color;
-        arregloBaraja[j].palo = naipeActual.palo;
-        arregloBaraja[j].numero = naipeActual.numero;
+  for (int j = 0; j < cartas_disponibles; j++) {
+    archivo_entrada.read((char *)(&arregloBaraja[j]), sizeof(Naipe));
+  }
 
-    }
-
-    archivo_entrada.close();
-    return arregloBaraja;
+  archivo_entrada.close();
+  return arregloBaraja;
 }
 
-/****************************************
-* Naipe robar_carta
-* **************************************
-* Toma una carta del arreglo, y la elimina de este.
-* **************************************
-*  Input:
-*      Naipe* arreglo   :   Arreglo de naipes (baraja)
-*      int &cartas_disponibles  :   Referencia de entero que contiene la cantidad de
-*                                   cartas disponibles
-* **************************************
-* Returns:
-*      Naipe carta_robada   :   Un naipe sacado del arreglo
-***************************************/
+Naipe robar_carta(Naipe *arreglo, int &cartas_disponibles) {
+  int indice = rand() % cartas_disponibles;
+  Naipe carta_robada = arreglo[indice];
 
-Naipe robar_carta(Naipe* arreglo, int& cartas_disponibles){
-    int indice = rand() % cartas_disponibles;
-    Naipe carta_robada = arreglo[indice];
-    for(int i = indice; i < cartas_disponibles - 1; i++){
-        arreglo[i] = arreglo[i + 1];
-    }
-    cartas_disponibles--;
-    return carta_robada;
+  arreglo[indice] = arreglo[cartas_disponibles - 1];
+  cartas_disponibles--;
+
+  return carta_robada;
 }
+int leer_carta(Naipe carta_robar) {
+  ifstream archivoBinario;
+  archivoBinario.open("baraja.dat", ios::binary);
 
-/****************************************
-* int color_carta
-* **************************************
-* Crea un contador de el color de los naipes
-* **************************************
-*  Input:
-*      Naipe carta  :   Naipe de donde obtener el color
-*      int cantidad_cartas  :   Entero que contiene la cantidad total de cartas
-* **************************************
-* Returns:
-*      colores : Arreglo de dos elementos que contiene la cantidad de negras y de rojas
-***************************************/
+  int cantidadNaipes;
+  archivoBinario.read((char *)&cantidadNaipes, sizeof(int));
+  int posicion;
 
-int color_carta (Naipe carta, int cantidad_cartas){
-    int Rojo = 0;
-    int Negro = 0;
-    for (int z = 0; z < cantidad_cartas; z++){
-        if (carta.color == "R"){
-            Rojo++;
-        } else {
-            Negro++;
-        }
+  Naipe naipeActual;
+  for (int a = 0; a < cantidadNaipes; a++) {
+    archivoBinario.read((char *)&naipeActual, sizeof(Naipe));
+    if (carta_robar.numero == naipeActual.numero ||
+        carta_robar.color == naipeActual.color ||
+        carta_robar.palo == naipeActual.palo) {
+      posicion = a;
     }
-    int colores[2];
-    colores[0] = Rojo;
-    colores[1] = Negro;
-    return colores;
+  }
+
+  archivoBinario.close();
+  return posicion;
 }
+int main() {
+  srand(time(nullptr));
+  ofstream salida;
+  int jugadores;
+  string juego;
 
-int main(){
-    ofstream archivo;
-    int jugadores;
-    string juego;
-    cout << "Ingrese la cantidad de jugadores: " << endl;
-    cin >> jugadores;
-    cout << "Ingrese el juego: " << endl;
-    cin >> juego;
-    string jugadores_string = to_string(jugadores);
+  cout << "Ingrese la cantidad de jugadores: " << endl;
+  cin >> jugadores;
+  cout << "Ingrese el juego: " << endl;
+  cin >> juego;
 
-    Naipe jugador[jugadores]; // Arreglo de naipes por cada jugador
+  if (juego == "P") {
+    int cartas_disponibles = 52;
 
-    if (juego == "P"){
-        Naipe* baraja = crear_baraja();
-        int cartas_repartir = 5;
-        archivo.open(juego + jugadores_string + ".txt", ios::binary);
-        if (jugadores < 2 or jugadores > 7){
-            cout << "La cantidad de jugadores no es valida" << endl;   
-            return 0;
-        }for (int i = 0; i < jugadores; i++){
-            for (int j = 0; j < cartas_repartir ; j++){
-                robar_carta(baraja, cartas_repartir);
-            }
-        }
+    salida.open(juego + to_string(jugadores) + ".txt");
+
+    Naipe *baraja = crear_baraja(cartas_disponibles);
+    int cartas_repartir = 5;
+
+    if (jugadores < 2 || jugadores > 8) {
+      cout << "La cantidad de jugadores no es valida" << endl;
+      delete[] baraja;
+      return 0;
     }
 
-    if(juego == "C"){
-        for (int a = 0; a < 2; a++){
-            Naipe* baraja = crear_baraja();
+    for (int i = 0; i < jugadores; i++) {
+
+      for (int j = 0; j < cartas_repartir; j++) {
+
+        Naipe carta_robada = robar_carta(baraja, cartas_disponibles);
+
+        salida << leer_carta(carta_robada) << " ";
+      }
+      salida << endl;
+    }
+  
+    salida.close();
+  }
+    if (juego == "B") {
+      int cartas_disponibles = 52;
+
+      salida.open(juego + to_string(jugadores) + ".txt");
+
+      Naipe *baraja = crear_baraja(cartas_disponibles);
+      int cartas_repartir = 4;
+
+      if (jugadores < 2 || jugadores > 7) {
+        cout << "La cantidad de jugadores no es valida" << endl;
+        delete[] baraja;
+        return 0;
+      }
+
+      for (int i = 0; i < jugadores; i++) {
+
+        for (int j = 0; j < cartas_repartir; j++) {
+
+          Naipe carta_robada = robar_carta(baraja, cartas_disponibles);
+
+          salida << leer_carta(carta_robada) << " ";
         }
+        salida << endl;
+      }
+      salida.close();
+    }
+      if (juego == "C") {
+
+        int cartas_disponibles = 104;
+
+        salida.open(juego + to_string(jugadores) + ".txt");
+
+        Naipe *baraja = crear_baraja(cartas_disponibles);
         int cartas_repartir = 15;
-        // son 2 barajas
-        archivo.open(juego + jugadores_string + ".txt", ios::binary);
-        if (jugadores < 2 or jugadores > 6){
-            cout << "La cantidad de jugadores no es valida" << endl;   
-            return 0;
-        }for (int i = 0; i < jugadores; i++){
 
+        if (jugadores < 2 || jugadores > 7) {
+          cout << "La cantidad de jugadores no es valida" << endl;
+          delete[] baraja;
+          return 0;
         }
-    }
 
-    if(juego == "B"){
-        Naipe* baraja = crear_baraja();
-        int cartas_repartir = 4;
-        archivo.open(juego + jugadores_string + ".txt", ios::binary);
-        if (jugadores < 2 or jugadores > 6){
-            cout << "La cantidad de jugadores no es valida" << endl;   
-            return 0;
-        }for (int i = 0; i < jugadores; i++){
-            robar_carta(baraja, cartas_repartir);
+        for (int i = 0; i < jugadores; i++) {
+
+            for (int j = 0; j < cartas_repartir; j++) {
+
+                Naipe carta_robada = robar_carta(baraja, cartas_disponibles);
+
+                salida << leer_carta(carta_robada) << " ";
+            }
+            salida << endl;
         }
-    }
+        salida.close();
+      
+        delete[] baraja;
+      }
+       else {
+        cout << "Juego no reconocido" << endl;
+      }
 
-    return 0;
-}
+      return 0;
+    }
+  
